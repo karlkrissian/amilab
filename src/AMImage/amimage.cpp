b3df97474b1680c1e63191936fdd3b9325972ec0
karl.krissian <karl.krissian@gmail.com>
2009-06-09 08:59:27 +0000

    using _mm_malloc and _mm_free from xmmintrin.h for image allocation and desallocation, but seems to give a problem still
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@171 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/AMImage/amimage.cpp
+++ src/AMImage/amimage.cpp
@@ -43,6 +43,8 @@
 #include <stdio.h>
 #include <locale.h>
 
+#include <xmmintrin.h>
+
 #include <boost/iostreams/copy.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <boost/regex.hpp>
@@ -95,9 +97,14 @@ extern unsigned char GB_verbose;
 #include "amilab_messages.h"
 
 #define NEW(mess,ptr,type,size) \
-    ptr = new type[size]; \
+    ptr = (type*) _mm_malloc (sizeof(type)*size,16); \
     CLASS_MESSAGE( boost::format("%1%, allocation of %2% Mb") \
         % mess % (1.0*size*sizeof(type)/1000000.0));
+
+#define DELETE(ptr) \
+  { \
+    CLASS_MESSAGE( " calling _mm_free() "); \
+   _mm_free(ptr); ptr = NULL; } \
 
 
 #define NUM_REPRES 12
@@ -552,8 +559,7 @@ amimage::~amimage()
 //        -------
 {
   if (data_allocated) {
-    delete [] (unsigned char*)data;
-    data = NULL;
+    DELETE(data);
   }
 }
 
@@ -564,8 +570,7 @@ unsigned char amimage::allocate()
 {
 
   if (data_allocated) {
-    delete [] (unsigned char*)data;
-    data = NULL;
+    DELETE(data);
   }
 
   this->ComputeDataSize();
@@ -953,7 +958,7 @@ unsigned char  amimage::readdata_ext( )
 
   if ( header_ptr!=NULL )
   {
-    delete [] header_ptr;
+    DELETE(header_ptr);
     header_ptr = NULL;
   }
 
