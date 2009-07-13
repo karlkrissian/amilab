a575521a2e8053b27db5c284dad539b9ec73e36f
karl.krissian <karl.krissian@gmail.com>
2009-07-13 09:24:08 +0000

    possibility to obtain the computed gradient field
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@256 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/AMILab/wrap_StructureTensor.cpp
+++ src/AMILab/wrap_StructureTensor.cpp
@@ -27,6 +27,7 @@ InrImage* wrap_StructureTensorHessianNew(ParamList* p)
               sigma       Gaussian convolution standard deviation \n\
               beta        weight of the square Hessian matrix \n\
               mask        mask image\n\
+              gradient    gradient image, get the computed smoothed gradient\n\
           Return:\n\
               A vectorial image representing the tensor as a symmetric matrix (6 components in 3D)\n\
             ";
@@ -35,6 +36,7 @@ InrImage* wrap_StructureTensorHessianNew(ParamList* p)
     float     sigma;
     float     beta;
     InrImage::ptr mask;
+    InrImage::ptr imgrad; // get the smoothed gradient
     int n=0;
 
 
@@ -47,7 +49,10 @@ InrImage* wrap_StructureTensorHessianNew(ParamList* p)
    mask = p->GetParamPtr<InrImage>(type_image,n++);
    if (mask==NULL) mask = InrImage::ptr();
 
-  InrImage* result = Func_StructureTensorHessianNew( input, sigma, beta, mask);
+   imgrad = p->GetParamPtr<InrImage>(type_image,n++);
+   if (imgrad==NULL) imgrad = InrImage::ptr();
+
+  InrImage* result = Func_StructureTensorHessianNew( input, sigma, beta, mask, imgrad);
 
 
   return result;
