2500fb38e8c758ab4c72adb8f14fcb078ac2b1ee
karl.krissian <karl.krissian@gmail.com>
2009-11-12 23:55:47 +0000

    merge from release-2.0.4 -r 373:head seems to have worked this time
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@379 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ src/Wrapping/WrapITK/wrapitkWaterShedImageFilter.cpp
@@ -0,0 +1,163 @@
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+#ifndef _WITHOUT_ITK_
+#include "itkWatershedImageFilter.h"
+#endif // _WITHOUT_ITK_
+
+#include "wrapfunctions.hpp" 
+#include "wrapConversion.tpp"
+#include "wrapitkWaterShedImageFilter.h"
+
+InrImage* itkWaterShedImageFilter2D(ParamList* p)
+{
+//	clock_t ITime = clock();
+
+#ifndef _WITHOUT_ITK_
+
+	char functionname[] = "itkWaterShedImageFilter2D";
+  char description[]=" \n\
+        Filter to compute a watershed solution in 2D.\n\
+      ";
+	  
+  char parameters[] =" \n\
+          Parameters:\n\
+          input image \n\
+          lowerThreshold \n\
+          outputScaleLevel \n\
+			";
+			
+	InrImage* input = NULL;
+	float lowerThreshold = 0.0;
+	float outputScaleLevel = 0.0;
+  InrImage* res = NULL;
+  int n=0;
+	
+	if (!get_image_param(  input,      p, n)) HelpAndReturnNULL;
+	if (!get_float_param(  lowerThreshold,      p, n)) HelpAndReturnNULL;
+	if (!get_float_param(  outputScaleLevel,      p, n)) HelpAndReturnNULL;
+	
+	typedef float               PixelType;
+  const   unsigned int        Dimension = 2;
+  typedef itk::Image< PixelType, Dimension >    ImageType;
+  ImageType::RegionType region;
+  ImageType::Pointer image;
+	
+	typedef itk::WatershedImageFilter<ImageType> WatershedFilterType;
+	
+	typedef unsigned long OutputPixelType;
+	typedef itk::Image< OutputPixelType,Dimension > OutputImageType;
+	OutputImageType::Pointer outimage;
+	
+	// Convert from InrImage to ITK
+  cout << "Converting image to ITK format " << endl;
+  	
+	image = InrToITK<PixelType,Dimension>(input,region);
+	
+	WatershedFilterType::Pointer watershed = WatershedFilterType::New();
+	// We set up the watershed filter. There are two parameters.
+  // Level controls watershed depth, and Threshold controls the
+  // lower thresholding of the input. Both parameters are set as a
+  // percentage (0.0 - 1.0) of the maximum depth in the input image.
+  watershed->SetLevel( outputScaleLevel );
+  watershed->SetThreshold( lowerThreshold );
+	watershed->SetInput(image);
+	watershed->Update();
+
+	outimage = watershed->GetOutput();
+   
+	// Convert from ITK to InrImage
+  cout << "Converting back to InrImage " << endl;
+
+	res = ITKToInr<OutputPixelType,Dimension>(outimage, region);
+//	cout << "Sale ?" << endl;
+//  clock_t FTime = clock();
+//  float seg = (FTime - ITime) / CLK_TCK;
+//  std::cout << "Time: " << seg << "seg" << std::endl;
+	return res;
+			
+#else
+  fprintf(stderr," ITK not available, you need to compile with ITK ...\n");
+  return NULL;
+#endif // _WITHOUT_ITK_	
+} //itkWaterShedImageFilter2D
+
+InrImage* itkWaterShedImageFilter3D(ParamList* p)
+{
+//	clock_t ITime = clock();
+
+#ifndef _WITHOUT_ITK_
+
+	char functionname[] = "itkWaterShedImageFilter3D";
+  char description[]=" \n\
+        Filter to compute a watershed solution in 3D.\n\
+      ";
+	  
+  char parameters[] =" \n\
+          Parameters:\n\
+          input image \n\
+          lowerThreshold \n\
+          outputScaleLevel \n\
+			";
+			
+	InrImage* input = NULL;
+	float lowerThreshold = 0.0;
+	float outputScaleLevel = 0.0;
+  InrImage* res = NULL;
+  int n=0;
+	
+	if (!get_image_param(  input,      p, n)) HelpAndReturnNULL;
+	if (!get_float_param(  lowerThreshold,      p, n)) HelpAndReturnNULL;
+	if (!get_float_param(  outputScaleLevel,      p, n)) HelpAndReturnNULL;
+	
+	typedef float               PixelType;
+  const   unsigned int        Dimension = 3;
+  typedef itk::Image< PixelType, Dimension >    ImageType;
+  ImageType::RegionType region;
+  ImageType::Pointer image;
+	
+	typedef itk::WatershedImageFilter<ImageType> WatershedFilterType;
+	
+	typedef unsigned long OutputPixelType;
+	typedef itk::Image< OutputPixelType,Dimension > OutputImageType;
+	OutputImageType::Pointer outimage;
+	
+	// Convert from InrImage to ITK
+  cout << "Converting image to ITK format " << endl;
+  	
+	image = InrToITK<PixelType,Dimension>(input,region);
+	
+	WatershedFilterType::Pointer watershed = WatershedFilterType::New();
+	// We set up the watershed filter. There are two parameters.
+  // Level controls watershed depth, and Threshold controls the
+  // lower thresholding of the input. Both parameters are set as a
+  // percentage (0.0 - 1.0) of the maximum depth in the input image.
+  watershed->SetLevel( outputScaleLevel );
+  watershed->SetThreshold( lowerThreshold );
+	watershed->SetInput(image);
+	watershed->Update();
+	
+	outimage = watershed->GetOutput();
+   
+	// Convert from ITK to InrImage
+  cout << "Converting back to InrImage " << endl;
+
+	res = ITKToInr<OutputPixelType,Dimension>(outimage, region);
+//	cout << "Sale ?" << endl;
+//  clock_t FTime = clock();
+//  float seg = (FTime - ITime) / CLK_TCK;
+//  std::cout << "Time: " << seg << "seg" << std::endl;
+	return res;
+			
+#else
+  fprintf(stderr," ITK not available, you need to compile with ITK ...\n");
+  return NULL;
+#endif // _WITHOUT_ITK_	
+} //itkWaterShedImageFilter3D
+
+			
