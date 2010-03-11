d32102428e36a6a8ff84cf089365aedd47c2a1aa
karl.krissian <karl.krissian@gmail.com>
2010-03-11 16:59:04 +0000

    adding vector of variable as an language object
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@511 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ src/Wrapping/WrapBasicTypes/wrap_varvector.cpp
@@ -0,0 +1,225 @@
+//
+// C++ Implementation: wrap_varvector
+//
+// Description: Wrapping of a vector of variables
+//
+//
+// Author: Karl krissian <>, (C) 2010
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+
+#include "VarContexts.hpp"
+#include "wrapfunctions.hpp"
+#include "wrapfunctions_draw.h"
+#include "ami_class.h"
+#include "ami_object.h"
+#include "wrap_varvector.h"
+
+
+#define RETURN_VARINT(val,name)             \
+  Variable::ptr varres(new Variable());\
+  std::string varname = (boost::format("%1%_id")%name).str();\
+  varres->Init(type_int,varname.c_str(),(void*) new int(val));\
+  return varres;
+
+
+extern VarContexts  Vars;
+
+AMIObject* AddWrapVarVector( const VarVector::ptr& objectptr)
+{
+  // Create new instance of the class
+  AMIObject* amiobject = new AMIObject;
+  amiobject->SetName("varvector");
+
+  // Set the object context
+  Variables::ptr previous_ocontext = Vars.GetObjectContext();
+  Vars.SetObjectContext(amiobject->GetContext());
+
+  ADDMEMBER("push_front",       VarVector, push_front);
+  ADDMEMBER("push_back",        VarVector, push_back);
+
+  ADDMEMBER("pop_front",       VarVector, pop_front);
+  ADDMEMBER("pop_back",        VarVector, pop_back);
+
+  ADDMEMBER("size",             VarVector, size);
+
+  ADDMEMBER("front",            VarVector, front);
+  ADDMEMBER("back",             VarVector, back);
+
+  ADDMEMBER("at",               VarVector, at);
+
+  // Restore the object context
+  Vars.SetObjectContext(previous_ocontext);
+
+  return amiobject;
+}
+
+//---------------------------------------------------
+Variable::ptr wrap_VarVector( ParamList* p)
+{
+    char functionname[] = "VarVector";
+    char description[]=" \n\
+      New VarVector object, store a vector of variables (smart pointers). \n\
+            ";
+    char parameters[] =" \n\
+      - no parameter \n\
+            ";
+
+  VarVector::ptr pp(new VarVector());
+
+  AMIObject* amiobject = AddWrapVarVector(pp);
+  Variable::ptr varres(new Variable());
+  varres->Init(type_ami_object,"tmp_varvector", (void*) amiobject);
+
+  return varres;
+
+}
+
+
+//---------------------------------------------------
+//  push_front
+//---------------------------------------------------
+void wrap_VarVectorpush_front::SetParametersComments() 
+{
+  ADDPARAMCOMMENT("variable: input variable to add.");
+}
+//---------------------------------------------------
+Variable::ptr wrap_VarVectorpush_front::CallMember( ParamList* p)
+{
+  
+  Variable::ptr var;
+  int   n = 0;
+
+  if (!get_generic_var_param(var, p, n))         ClassHelpAndReturn;
+  // probably still "naive" implementation, need to check for the permanence of the smart pointer ?
+  // or is it ok?
+  // do we want explicit copy or reference of the variable contents ??
+  this->_objectptr->vector.push_front(var);
+
+  return Variable::ptr();
+}
+
+//---------------------------------------------------
+//  push_back
+//---------------------------------------------------
+void wrap_VarVectorpush_back::SetParametersComments() 
+{
+  ADDPARAMCOMMENT("variable: input variable to add.");
+}
+//---------------------------------------------------
+Variable::ptr wrap_VarVectorpush_back::CallMember( ParamList* p)
+{
+  
+  Variable::ptr var;
+  int   n = 0;
+
+  if (!get_generic_var_param(var, p, n))         ClassHelpAndReturn;
+  // probably still "naive" implementation, need to check for the permanence of the smart pointer ?
+  // or is it ok?
+  // do we want explicit copy or reference of the variable contents ??
+  this->_objectptr->vector.push_back(var);
+
+  return Variable::ptr();
+}
+
+
+//---------------------------------------------------
+//  pop_front
+//---------------------------------------------------
+void wrap_VarVectorpop_front::SetParametersComments() {}
+//---------------------------------------------------
+Variable::ptr wrap_VarVectorpop_front::CallMember( ParamList* p)
+{
+  this->_objectptr->vector.pop_front();
+  return Variable::ptr();
+}
+
+
+//---------------------------------------------------
+//  pop_back
+//---------------------------------------------------
+void wrap_VarVectorpop_back::SetParametersComments() {}
+//---------------------------------------------------
+Variable::ptr wrap_VarVectorpop_back::CallMember( ParamList* p)
+{
+  this->_objectptr->vector.pop_back();
+  return Variable::ptr();
+}
+
+
+//---------------------------------------------------
+//  size
+//---------------------------------------------------
+void wrap_VarVectorsize::SetParametersComments() 
+{
+  return_comments = "Vector size (int variable).";
+}
+//---------------------------------------------------
+Variable::ptr wrap_VarVectorsize::CallMember( ParamList* p)
+{
+  int size = this->_objectptr->vector.size();
+  // create integer variable to return
+  RETURN_VARINT(size,"Vector_size");
+}
+
+
+//---------------------------------------------------
+//  front
+//---------------------------------------------------
+void wrap_VarVectorfront::SetParametersComments() 
+{
+  return_comments = "Vector first element (variable).";
+}
+//---------------------------------------------------
+Variable::ptr wrap_VarVectorfront::CallMember( ParamList* p)
+{
+  if (this->_objectptr->vector.empty()) 
+    return Variable::ptr();
+  else
+    return this->_objectptr->vector.front();
+}
+
+//---------------------------------------------------
+//  back
+//---------------------------------------------------
+void wrap_VarVectorback::SetParametersComments() 
+{
+  return_comments = "Vector last element (variable).";
+}
+//---------------------------------------------------
+Variable::ptr wrap_VarVectorback::CallMember( ParamList* p)
+{
+  if (this->_objectptr->vector.empty()) 
+    return Variable::ptr();
+  else
+    return this->_objectptr->vector.back();
+}
+
+
+//---------------------------------------------------
+//  at
+//---------------------------------------------------
+void wrap_VarVectorat::SetParametersComments() 
+{
+  ADDPARAMCOMMENT("int   : vector position");
+  return_comments = "Element at the given location if valid (variable).";
+}
+//---------------------------------------------------
+Variable::ptr wrap_VarVectorat::CallMember( ParamList* p)
+{
+
+  int pos = 0;
+  int n=0;
+  if (!get_int_param(pos, p, n)) ClassHelpAndReturn;
+
+  if ((pos<0)&&(pos>=this->_objectptr->vector.size())) 
+    return Variable::ptr();
+  else
+    return this->_objectptr->vector.at(pos);
+}
+
+
+
---
+++ src/Wrapping/WrapBasicTypes/wrap_varvector.h
@@ -0,0 +1,47 @@
+//
+// C++ Interface: wrap_varvector
+//
+// Description: Wrapping of a list of variables
+//
+//
+// Author: Karl krissian  <>, (C) 2010
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef _wrap_varvector_h_
+#define _wrap_varvector_h_
+
+#include "wrapfunction_class.h"
+#include "Variable.hpp"
+#include "paramlist.h"
+#include "DefineClass.hpp"
+#include <vector>
+
+// varvector type
+class VarVector
+{
+  DEFINE_CLASS(VarVector);
+
+  protected:
+    std::vector<Variable::ptr> vector;
+};
+
+/** function that add wrapping of the Image Drawing window
+ */
+Variable::ptr wrap_VarVector( ParamList* p);
+
+ADD_METHOD( VarVector, push_front,        "Add element at beginning.");
+ADD_METHOD( VarVector, push_back,         "Add element at the end.");
+ADD_METHOD( VarVector, pop_front,         "Deletes the element at the beginning of a list.");
+ADD_METHOD( VarVector, pop_back,          "Deletes the element at the end of a list.");
+ADD_METHOD( VarVector, size,              "Return size.");
+ADD_METHOD( VarVector, front,             "Access first element.");
+ADD_METHOD( VarVector, back,              "Access last element.");
+
+ADD_METHOD( VarVector, at,                "Return the value a the given position.");
+
+
+#endif
+// _wrap_varvector_h_
