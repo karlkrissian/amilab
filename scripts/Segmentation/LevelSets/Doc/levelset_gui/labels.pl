891f5effb3eeb1cfb11924a6a3ce3e97994be27f
karl.krissian <karl.krissian@gmail.com>
2010-04-12 17:57:48 +0000

    added doc of level sets
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@541 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ scripts/Segmentation/LevelSets/Doc/levelset_gui/labels.pl
@@ -0,0 +1,45 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_Sethian99book/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_OsherParagios2003/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Sapiro01/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:GenLS/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Osher2002/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:LS_terms/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/eq:GenLS/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:LS_terms/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+1;
+
