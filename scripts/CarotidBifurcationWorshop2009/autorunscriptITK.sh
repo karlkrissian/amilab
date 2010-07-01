e746bd0e2104af16b59f1e991824907df37fccdf
Sara Arencibia <unknownmail.com>
2010-07-01 09:22:31 +0000

    prueba
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/branches/PFCSaraNew@770 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- scripts/CarotidBifurcationWorshop2009/autorunscriptITK.sh
+++ scripts/CarotidBifurcationWorshop2009/autorunscriptITK.sh
@@ -7,5 +7,5 @@ shift
 for n in $*
 do
   /home/sara/Pruebas\ Workshop/build/amilab/amilab_2.1.0_release autorunITK.amil $center $n
-  /home/sara/Pruebas\ Workshop/build/amilab/amilab_2.1.0_release autorunANT.amil $center $n
+  #/home/sara/Pruebas\ Workshop/build/amilab/amilab_2.1.0_release autorunANT.amil $center $n
 done
