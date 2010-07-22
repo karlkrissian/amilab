bf4b5cb539d56c23a4a49af1fd4eaf9b6e707713
karl.krissian <karl.krissian@gmail.com>
2010-07-22 15:25:11 +0000

    fixed some problems while trying on Mac
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@812 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/wxCode/wxTreeListCtrl/treelistctrl.cpp
+++ src/wxCode/wxTreeListCtrl/treelistctrl.cpp
@@ -45,7 +45,7 @@
 #include <wx/apptrait.h>
 
 #ifdef __WXMAC__
-#include "wx/mac/private.h"
+//#include "wx/mac/private.h"
 #endif
 
 #include "treelistctrl.h"
@@ -1875,7 +1875,7 @@ bool wxTreeListMainWindow::Create (wxTreeListCtrl *parent,
                                    const wxString& name) {
 
 #ifdef __WXMAC__
-    if (style & wxTR_HAS_BUTTONS) style |= wxTR_MAC_BUTTONS;
+//    if (style & wxTR_HAS_BUTTONS) style |= wxTR_MAC_BUTTONS;
     if (style & wxTR_HAS_BUTTONS) style &= ~wxTR_HAS_BUTTONS;
     style &= ~wxTR_LINES_AT_ROOT;
     style |= wxTR_NO_LINES;
