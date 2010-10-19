f3f449cb48b37fcaef3edad330c8cff6447941bf
karl.krissian <karl.krissian@gmail.com>
2010-10-19 00:11:48 +0000

    Improving several scripts
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@986 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- scripts/CarotidBifurcationWorkshop2009/RunExperimentsITK/Doc/carotid/internals.pl
+++ scripts/CarotidBifurcationWorkshop2009/RunExperimentsITK/Doc/carotid/internals.pl
@@ -1,13 +1,13 @@
-# LaTeX2HTML 2002-2-1 (1.71)
+# LaTeX2HTML 2008 (1.71)
 # Associate internals original text with physical files.
 
 
 $key = q/cite_CLS/;
-$ref_files{$key} = "$dir".q|node17.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_IJ/;
-$ref_files{$key} = "$dir".q|node17.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 1;
