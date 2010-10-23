0f9455a62cfd385556933950642edbddac639dca
karl.krissian <karl.krissian@gmail.com>
2010-10-23 09:55:27 +0000

    New linux installation scripts from source code
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1014 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ LinuxInstall/CMAKE_COMPILE.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# this script is designed to run within the install_linux.sh script
+# but could run as standalone if all the needed commands are available
+#
+
+SITE="http://www.cmake.org/files/v2.8/"
+FILE="cmake-2.8.2-Linux-i386.tar.gz"
+
+if [ ! -z "$CMAKE_PRECOMP" ];
+then
+  $INSTALLCMD $CMAKE_PRECOMP
+fi
+
+# if not already downloaded, install it
+if [ \! -f $FILE ];
+then
+  wget ${SITE}${FILE}
+  tar zxf $FILE
+  cp cmake-2.8.2-Linux-i386/bin/cmake /usr/local/bin/
+  cp cmake-2.8.2-Linux-i386/bin/cpack /usr/local/bin/
+  # for the rest of the script, we need to use /usr/local/bin for cmake ??
+  # or is it automatic ?
+fi
