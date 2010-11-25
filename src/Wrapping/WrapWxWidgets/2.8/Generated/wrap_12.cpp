bf20da97a3e93e2a9cbd6312070f9a72a3cff78a
Francisco Santana <unknownmail.com>
2010-11-25 14:12:25 +0000

    Fixed src/Wrapping/WrapWxWidgets/CMakeLists.txt
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1089 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/Wrapping/WrapWxWidgets/2.8/Generated/wrap_12.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "wrap_wxWindowBase.cpp"
-#include "wrap_wxWindow.cpp"
-#include "wrap_wxWindowList.cpp"
-#include "wrap_wxWindowListNode.cpp"
-#include "wrap_wxWritableCharBuffer.cpp"
-#include "wrap_wxWritableWCharBuffer.cpp"
-#include "wrap_wxwxMenuItemListNode.cpp"
-#include "wrap_wxwxSizerItemListNode.cpp"
