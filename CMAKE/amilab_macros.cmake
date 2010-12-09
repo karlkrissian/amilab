c3342ab9b3769e7554f7575af70bcb27e664aac7
Francisco Santana <unknownmail.com>
2010-12-09 22:08:37 +0000

    Added GenerateSourcesWrapping macro (pending tests)
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1129 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- CMAKE/amilab_macros.cmake
+++ CMAKE/amilab_macros.cmake
@@ -39,3 +39,145 @@ MACRO( my_add_library var)
   PrintSources(${var})
 ENDMACRO(my_add_library)
 #-------------------------------------------------------------------------------
+
+
+
+#-------------------------------------------------------------------------------
+# Specifies the number of sources that will be packed in a file. By default, 10
+# files are packaged.
+#-------------------------------------------------------------------------------
+SET(NUMBER_SOURCES_TO_PACKAGE 9 CACHE STRING "Indicates the number of sources to package")
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Creates a directory. If the directory exists, it does not do anything.
+#-------------------------------------------------------------------------------
+MACRO( CreateDirectory path)
+#-------------------------------------------------------------------------------
+  IF(EXISTS ${path})
+    MESSAGE("Directory ${path}: Exists")
+  ELSE(EXISTS ${path})
+    MESSAGE("Directory ${path}: Create")
+    FILE(MAKE_DIRECTORY ${path})
+  ENDIF(EXISTS ${path})
+ENDMACRO(CreateDirectory)
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Determines if a file exists. If not, it creates a certain content.
+#-------------------------------------------------------------------------------
+MACRO( FileExists file result body)
+#-----------------------------------------------------------------------------
+  IF(EXISTS ${file})
+    SET(${result} TRUE)
+  ELSE(EXISTS ${file})
+    FILE(WRITE ${file} ${body})
+    SET(${result} FALSE)
+  ENDIF(EXISTS ${file})
+ENDMACRO(FileExists)
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+MACRO( GenerateSourcesWrapping new_code_list path code_list )
+#-------------------------------------------------------------------------------
+  SET(source_code_list "${code_list}")
+  SET(source_code_path "${path}")
+
+  #MESSAGE("source code: ${code_list}\n\npath: ${source_code_path}")
+
+  #-----------------------------------------------------------------------------
+  # 1. Verify if there exists the output_dir_path directory. If the directory
+  #    does not exist, it creates it.
+  #-----------------------------------------------------------------------------
+  SET(output_dir_path "${source_code_path}/Generated")
+  CreateDirectory(${output_dir_path})
+
+  #-----------------------------------------------------------------------------
+  # 2. Verify if there exists the wrap_source_list.txt file. If the file does
+  #    not exist, it creates it with the content of the source_code_list
+  #    variable.
+  #-----------------------------------------------------------------------------
+  SET(wrap_source_filename "${output_dir_path}/wrap_source_list.txt")
+  SET(result FALSE)
+  FileExists(${wrap_source_filename} result ${source_code_list} )
+  MESSAGE("result: ${result}")
+
+  #-------------------------------------------------------------------------------
+  # 3. Check if it begins the process. Reads the content of the file
+  #    wrap_source_filename and compares it with the content of the variable
+  #    source_code_list. If they are equal, the process does not begin.
+  #-------------------------------------------------------------------------------
+  IF(result) #There has been a previous generation of files wrap_x.cpp
+    FILE(READ ${wrap_source_filename} wrap_source_list)
+    IF(wrap_source_list EQUAL source_code_list)
+      SET(build_flag FALSE)
+    ELSE(wrap_source_list EQUAL source_code_list)
+      SET(build_flag TRUE)
+    ENDIF(wrap_source_list EQUAL source_code_list)
+  ELSE(result)
+    SET(build_flag TRUE)
+  ENDIF(result)
+
+  #-----------------------------------------------------------------------------
+  # 4. Checks if the process begins.
+  #-----------------------------------------------------------------------------
+  IF(build_flag)
+
+    #---------------------------------------------------------------------------
+    # 4.1. Preprocess the source file list.
+    #---------------------------------------------------------------------------
+    SET(output_src "")
+    FOREACH(loop_var ${source_code_list})
+      SET(include_header "#include \"${loop_var}\"")
+      SET(output_src ${output_src} ${include_header})
+    ENDFOREACH(loop_var)
+
+    #---------------------------------------------------------------------------
+    # 4.2. Process the list.
+    #---------------------------------------------------------------------------
+    SET(counter_files 0)
+    LIST(LENGTH output_src output_src_length)
+    SET(new_source_list "")
+    MESSAGE("size: ${output_src_length}")
+    WHILE(output_src_length)
+
+      #-------------------------------------------------------------------------
+      # 4.2.1. Generate packages of NUMBER_SOURCES_TO_PACKAGE files.
+      #-------------------------------------------------------------------------
+      SET(include_list "")
+      FOREACH(counter RANGE ${NUMBER_SOURCES_TO_PACKAGE})
+
+        IF(output_src_length)
+          LIST(GET output_src -1 element_src)
+          SET(include_list "${include_list} ${element_src}\n")
+          LIST(REMOVE_AT output_src -1)
+          LIST(LENGTH output_src output_src_length)
+        ENDIF(output_src_length)
+
+      ENDFOREACH(counter)
+
+      #-------------------------------------------------------------------------
+      # 4.2.2. Generate the wrap files.
+      #-------------------------------------------------------------------------
+      SET(name_output "wrap_${counter_files}.cpp")
+      MATH(EXPR counter_files "${counter_files}+1")
+
+      FILE(WRITE "${output_dir_path}/${name_output}" ${include_list})
+      SET(new_source_list ${new_source_list} "${output_dir_path}/${name_output}")
+
+    ENDWHILE(output_src_length)
+
+    #---------------------------------------------------------------------------
+    # 4.3. Obtain the necessary to compile source code listing and file with the
+    #      original source code.
+    #---------------------------------------------------------------------------
+    SET(${new_code_list} ${new_source_list})
+    FILE(WRITE ${wrap_source_filename} ${new_source_list})
+
+  ELSE(build_flag)
+    FILE(GLOB wx_SRCS_list  "${output_dir_path}/*.cpp")
+    SET(${new_code_list} ${wx_SRCS_list})
+  ENDIF(build_flag)
+
+ENDMACRO( GenerateSourcesWrapping)
+#-------------------------------------------------------------------------------
