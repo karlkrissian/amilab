4cd8c374ad4c8c222ba0dd2a5d8d2cf0a8101c0e
karl.krissian <karl.krissian@gmail.com>
2011-01-19 18:22:35 +0000

    small fix
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1199 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- PythonWrap/parse_xml/wrap_class.py
+++ PythonWrap/parse_xml/wrap_class.py
@@ -376,7 +376,7 @@ class ParsePublicMembers:
       # problem: for structure, the constructor name is something like '._12': not a valid nor usefull name:
       # replace it by the class or structure name ...
       # only if not template ... so check for '<' character
-      if mname != config.types[context].GetString() and mname.find("<")!=-1:
+      if mname != config.types[context].GetString() and mname.find("<")==-1:
         utils.WarningMessage(" replacing constructor name {0} --> {1}".format(mname,config.types[context].GetString()))
         mname = config.types[context].GetString()
       self.CheckMethodName(self.public_members.ConstructorNames,self.public_members.Constructors,mname)
