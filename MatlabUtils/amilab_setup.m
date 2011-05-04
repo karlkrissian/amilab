9cb67d6e1f6afd908695da173445d3177bafe497
karl.krissian <karl.krissian@gmail.com>
2011-05-04 11:24:48 +0000

    improved matlab utilities
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1312 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ MatlabUtils/amilab_setup.m
@@ -0,0 +1,12 @@
+function amilab_setup()
+%
+% Amilab global configuration variables
+%
+  global amilab_tmpdir
+  global amilab_bindir
+  global amilab_bin
+
+  amilab_tmpdir='C:\Users\Karl\Documents\';
+  amilab_bindir='C:\Program Files\AMILab_3.1.0\bin';
+  amilab_bin='amilab_3.1.0.exe'
+ 
