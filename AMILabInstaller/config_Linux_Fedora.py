65baa98ecc3034c2684064d5a53ba7f6d2a5842a
karl.krissian <karl.krissian@gmail.com>
2011-10-30 21:30:49 +0000

    fix for fedora compilation
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1482 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- AMILabInstaller/config_Linux_Fedora.py
+++ AMILabInstaller/config_Linux_Fedora.py
@@ -2,4 +2,6 @@
 import config
 
 def SetConfig():
-  config.PYSVN_PACKAGES="pysvn"
+  config.PYSVN_PACKAGES         ="pysvn"
+  # not sure what package creates the need for libxml2-devel 
+  config.OTHERDEV_PACKAGES      += " libxml2-devel"
