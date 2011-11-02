ea77dc6d1f6c77e45468b3370a58512ac67b1c7f
karl.krissian <karl.krissian@gmail.com>
2011-11-02 18:21:35 +0000

    added Ubuntu configuration
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1485 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ AMILabInstaller/config_Linux_Ubuntu.py
@@ -0,0 +1,28 @@
+import config
+import os
+
+def PreInstall():
+  print "--------------------------------------"
+  print " Installing packagekit"
+  print "--------------------------------------"
+  os.system("sudo apt-get install packagekit")
+
+def SetConfig():
+  config.PYSVN_PACKAGES       = "pysvn"
+  config.INSTALLCMD           = "apt-get install"
+  config.INSTALLER_PACKAGES   = "" 
+  config.COMPILATION_PACKAGES = " build-essential subversion wget tar gawk"
+  config.CMAKE_PACKAGES       = "cmake cmake-curses-gui cmake-qt-gui"
+  config.VTKDEV_PACKAGES      = "libvtk5-dev"
+  # installing libgtk2.0-dev because of wxVTK class
+  config.WXWIDGETSDEV_PACKAGES= "libwxgtk2.8-dev libgtk2.0-dev"
+  config.BOOSTDEV_PACKAGES    = "libboost-all-dev"
+  config.IMDEV_PACKAGES       = "libpng12-dev libtiff4-dev"
+  config.OTHERDEV_PACKAGES    = "libexpat1-dev  uuid-dev libgdcm2-dev"
+  config.COMPRESSDEV_PACKAGES = "zlib1g-dev libbz2-dev"
+  config.BISONFLEX_PACKAGES   = "bison flex"
+  config.ITKDEV_PACKAGES      = "TOCOMPILE"
+  #config.ITKDEV_PACKAGES      = "libinsighttoolkit3-dev"
+  # AMILab cmake configuration flags
+  config.AMILAB_CMAKE_FLAGS   = "-DCMAKE_BUILD_TYPE=Release  -DAMI_USE_STATIC_LIBS=OFF"
+  config.PYSVN_PACKAGES       = "python-svn"
---
+++ AMILabInstaller/config_Linux_Ubuntu_11_10.py
@@ -0,0 +1,9 @@
+import config
+import os
+
+
+def SetConfig():
+  # nothing special to do here
+  print "nothing special for version 11.10"
+
+
