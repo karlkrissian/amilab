f79b537efaf733b6e8a590f756b64b69b210dbac
karl.krissian <karl.krissian@gmail.com>
2011-11-04 16:01:24 +0000

    getting back config.py file deleted by mistake
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1493 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ AMILabInstaller/config.py
@@ -0,0 +1,20 @@
+
+# default configuration
+# will be specified to each system
+
+INSTALLCMD            = ""
+INSTALLER_PACKAGES    = ""
+COMPILATION_PACKAGES  = "gcc gcc-c++ binutils make subversion wget tar gawk"
+CMAKE_PACKAGES        = "cmake cmake-gui"
+VTKDEV_PACKAGES       = "vtk-devel"
+WXWIDGETSDEV_PACKAGES = "wxGTK-devel"
+BOOSTDEV_PACKAGES     = "boost-devel"
+IMDEV_PACKAGES        = "libpng-devel libtiff-devel libjpeg-devel"
+OTHERDEV_PACKAGES     = "expat-devel"
+COMPRESSDEV_PACKAGES  = "zlib-devel bzip2-devel"
+BISONFLEX_PACKAGES    = "bison flex"
+ITKDEV_PACKAGES       = "TOCOMPILE"
+CVS_PACKAGES          = "cvs"
+GCCXML_PACKAGES       = "gccxml"
+AMILAB_CMAKE_FLAGS    = "-DCMAKE_BUILD_TYPE=Release  -DAMI_USE_STATIC_LIBS=OFF"
+PYSVN_PACKAGES        = "pysvn"
