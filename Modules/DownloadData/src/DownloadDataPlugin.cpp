4c82f7c8d909a8f1fce6f0a62a3724d5b054c92d
karl.krissian <karl.krissian@gmail.com>
2011-11-25 10:03:42 +0000

    Added DownloadData module
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1554 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ Modules/DownloadData/src/DownloadDataPlugin.cpp
@@ -0,0 +1,54 @@
+//
+// C++ File:Plugin.cpp
+//
+// Description: A plugin example.
+//
+//
+// Author: Author: xXx <xXx@xXx.com>, (C) YYYY
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+
+#include "DownloadDataPlugin.h"
+#include "VarContexts.hpp"
+#include "ami_object.h"
+#include "LanguageBaseConfigure.h"
+LanguageBase_VAR_IMPORT VarContexts Vars;
+
+#include "addwrap_DownloadData.h"
+
+
+
+PLUGIN_ENTRY_FUNCTION(DownloadDataPlugin);
+
+DownloadDataPlugin::DownloadDataPlugin()
+{
+  SetName("DownloadData");
+  SetDescription("Several algorithms for analysis of noise in images.");
+  SetAuthor("...");
+  SetVersion("1.0.0");
+}
+
+bool DownloadDataPlugin::Execute(void)
+{
+  // Create a new context (or namespace)
+  AMIObject::ptr amiobject(new AMIObject);
+  amiobject->SetName("DownloadData");
+
+   // wrap_DownloadData_classes(  amiobject->GetContext());
+
+  // Add wx context to builtin
+  // Could be in global with a default context
+  Vars.GetBuiltinContext()->AddVar<AMIObject>( amiobject->GetName().c_str(),
+      amiobject,Vars.GetBuiltinContext());  
+
+  // Add manually wrapped classes here
+    // Putting DownloadData and SubVoxel classes here in Algorithms
+     wrap_DownloadData_classes(  amiobject->GetContext());
+
+
+  // Set as default
+  Vars.GetBuiltinContext()->AddDefault(amiobject->GetContext());
+
+  return true;
+}
---
+++ Modules/DownloadData/src/Wrapping/DownloadData_includes.h
@@ -0,0 +1,5 @@
+
+#include <wx/url.h>
+#include <wx/stream.h>
+#include <wx/wfstream.h>
+#include <wx/sstream.h>
