3178ae946f826299b94c2e062d3d6c6a69236575
karl.krissian <karl.krissian@gmail.com>
2011-12-09 19:40:32 +0000

    improving GraphViz script and wxStcFrame for amilab and dot support
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1586 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/Graphic/wxStcFrame/prefs.cpp
+++ src/Graphic/wxStcFrame/prefs.cpp
@@ -96,6 +96,120 @@ const wxChar* PythonWordlist2 =
     _T("RADIOBUTTON RCDATA RTEXT SCROLLBAR SEPARATOR SHIFT STATE3 ")
     _T("STRINGTABLE STYLE TEXTINCLUDE VALUE VERSION VERSIONINFO VIRTKEY");
 
+    
+const wxChar* AMILabWordlist1 =
+    _T("PathFromDispl InvertNormals sin GetObjectName end BorgeforsSDT tan ")
+    _T("HessianMatrix Image DrawAllContours BrowseImage CircleInt AnisoWeickert ")
+    _T("AddPoint vtkSignedFMDist SetIsoContourColor ConvexHull ")
+    _T("vtkPropDanielsson SetWindowSize Transform SetVectParam subimage ")
+    _T("LineAddPointNumber rewind secdergrad SelectLines SetLightSpecular CC ")
+    _T("global CircleIntSdExc func EndLine PropagationDist2 StructTensorH ")
+    _T("exists EDPdilate Convolve del PropagationDist GetXmax else OrientField ")
+    _T("Normalize PaintCallback proc sh filter itk isosurf_inv AnisoSRAD ")
+    _T("SetCompTransf Displace cosh vtkSmooth DisplayHisto TraceScanning ")
+    _T("putimage TRANSFORM sum repeat PropDanielsson SetVerboseOff ")
+    _T("import_filters read Quit step Comments TraceParsing HessianEVal ")
+    _T("ResampleLines Paint amiOFCorr2D setvector vtkFastMarching asin DOUBLE ")
+    _T("ln end-of-line discsecdergrad while ls MeanHalfSize GetName DrawVector ")
+    _T("vtkIsoContourDist GetLinesLength ShortestPath ElevateMesh abs ")
+    _T("LineNumber compare in WriteCTALine SetColorMaterial Eigen3D ")
+    _T("GetNumberOfLines GetYmax TimeSpent vtkMINCRead autocrop median STRING ")
+    _T("setGLwin eval AndreDist vtkDist NbPolys CannyEdgeDetector atan ")
+    _T("localmean SRAD_qcoeff UINT IsosurfDist normgrad Set3DArrowParam Xpos ")
+    _T("CreateVolume GenRead Pi Remove discnormgrad rotate break ")
+    _T("SetPointsColors InitTime tz ty tx vtkAnisoGS ReadCTALine OBJECT ")
+    _T("Center open vtkSignedBorgefors Laplacian Matrix Surface Chamfer2DT ")
+    _T("SetStatusText ConnectLines PrintMatrices Ypos SetLightDiffuse ")
+    _T("OutFlux set NUM SetColor global_new CreateVectorsFromField ")
+    _T("AnisoSmoothGS SetIsoContourParam vtkDecimate SetGetExtent AnisoDPAD ")
+    _T("trz save SetLight cumhisto SetSpecular SetShininess EDPclose inc ")
+    _T("SetOpacity CreateVectors until CreateFlatMesh sprint sqrt sinh min ")
+    _T("ProcessXEvents AnisoNRAD AddGaussNoise Eigen2D vtkPropDaniel2 ")
+    _T("SetColorOpacity ConvolveMask UCHAR drawcircle OpImage AnisoRudinMult ")
+    _T("setminCC trx try SURFACE drawCC GetLine import_wii FluxDiffusion exp ")
+    _T("ReSlice DisplayVectors OwnMaterial AnisoCW Translate SNR ")
+    _T("CircleMinIntSdExc InfoDialog IMAGE ShowSection ShortestPathImage ")
+    _T("floor Triangulate local SetColors OrientationRatio isosurf_ras mean ")
+    _T("IsocontourPoints NbPoints showcursor LineRecons norm SetLineWidth ")
+    _T("Help count round SaveStructuredGrid SetFluidNavFile SetCurrentObj ")
+    _T("SetZoom RemoveLine scan_float SetLightPos SpacePos for Zpos NULL ")
+    _T("initvalue lininterp SetLineThickness GetZmin AddMenuScript ")
+    _T("Curvatures emptyargs GetYmin update gradient SetProgress cos ")
+    _T("setpos SetDiffuse isosurf EDPerode vars GetLinesExtremities val ")
+    _T("SetVerboseOn endfor setvoxelsize init Skeleton Pad SetComponent ")
+    _T("import_vtk rot2D Normals posarea AllContoursParam vtkCreateFlatMesh ")
+    _T("getimage SetName printn boost_format EndTime vdim NewLine ")
+    _T("Skeleton2lines log EDPopen SetLightAmbient Interpolate Statistics ")
+    _T("ReadFlow DirConnectivity SetColormap AnisoLeeAdd2 GetImageFromX ")
+    _T("SetBackground ChamferDT SetAmbient DrawEarthCoord if eccentricity ")
+    _T("MergePoints ReadRawImages OrientPositive SetDebugOff SHORT ")
+    _T("SetVisible DiscMeanCurvature schedule GetZPos Resize ")
+    _T("vtkMarchingCubes Scale GetTransform SetVectStyle RGB delvars ")
+    _T("FileExists local_new slice setendianness DrawIsoContour ")
+    _T("ProcessEvents OutFluxScalar argmax close ComputeAltitudes ")
+    _T("computeCC amiOFVar2D max SetCompareDisplacement GetScriptDir ")
+    _T("threscross Recompute Hide wait SimplePoints localSD ")
+    _T("vtkFastMarchingTarget GeoCoordinates FLOAT_VECTOR print ")
+    _T("GetZmax INT LocalExtrema StructTensor localmean2 PathFromVectField ")
+    _T("acos normalize vtkDicomRead settranslation SetVectColor Invert ")
+    _T("GetConnections BorgeforsDT SmoothLines GetXmin CHAR GetYPos info ")
+    _T("vy vx vz gnuplot secdergrad2 CreateEllipsoids GetIntensities ")
+    _T("AddObject isoarea2D NormalField AnisoSRAD2 GetXPos Flip ")
+    _T("SetIsoContour vtkWindowedSinc AnisoDPAD2 SetOpacityImage argc ")
+    _T("SetDebugOn USHORT Derivatives vtkMedian3D Temp2Altitudes ")
+    _T("HideSection IMAGEDRAW SetTransform Class GetFormat CircleIntExc ")
+    _T("SwapBuffers ReadCTALineRadii iterate to Altitude2Position Show ")
+    _T("RemoveSelection RestartTime histo isoline atof localSD2 isosurf_ijk ")
+    _T("ParamWin FLOAT");
+    
+
+// TODO: read xml description ...
+// for the moment try to make it work with keywords ...
+const wxChar* DOTWordlist1 =
+    _T("node graph digraph strict edge subgraph");
+
+const wxChar* DOTWordlist2 =
+    _T("Mcircle Mdiamond Mrecord Msquare TRUE auto back bold both box circle ")
+    _T("compress dashed diamond dot dotted doublecircle doubleoctagon egg ")
+    _T("ellipse epsf false fill filled forward global hexagon house inv ")
+    _T("invdot invhouse invis invodot invtrapezium invtriangle local max ")
+    _T("min none normal octagon odot out parallelogram plaintext polygon ")
+    _T("record same solid trapezium triangle tripleoctagon true");
+    
+const wxChar* DOTWordlist3 =
+    _T("aliceblue antiquewhite aquamarine azure beige bisque black ")
+    _T("blanchedalmond blue blueviolet brown burlywood cadetblue chartreuse ")
+    _T("chocolate coral cornflowerblue cornsilk crimson cyan darkgoldenrod ")
+    _T("darkgreen darkkhaki darkolivegreen darkorange darkorchid darksalmon ")
+    _T("darkseagreen darkslateblue darkslategray darkturquoise darkviolet ")
+    _T("deeppink deepskyblue dimgray dodgerblue firebrick forestgreen ")
+    _T("gainsboro ghostwhite gold goldenrod gray green greenyellow honeydew ")
+    _T("hotpink indianred indigo ivory khaki lavender lavenderblush ")
+    _T("lawngreen lemonchiffon lightblue lightcyan lightgoldenrod ")
+    _T("lightgoldenrodyellow lightgray lightpink lightsalmon lightseagreen ")
+    _T("lightskyblue lightslateblue lightslategray lightyellow limegreen ")
+    _T("linen magenta maroon mediumaquamarine mediumblue mediumorchid ")
+    _T("mediumpurple mediumseagreen mediumslateblue mediumspringgreen ")
+    _T("mediumturquoise mediumvioletred midnightblue mintcream mistyrose ")
+    _T("moccasin navajowhite navy navyblue oldlace olivedrab oralwhite ")
+    _T("orange orangered orchid palegoldenrod palegreen paleturquoise ")
+    _T("palevioletred papayawhip peachpuff peru pink plum powderblue ")
+    _T("purple red rosybrown royalblue saddlebrown salmon salmon2 sandybrown ")
+    _T("seagreen seashell sienna skyblue slateblue slategray snow ")
+    _T("springgreen steelblue tan thistle tomato turquoise violet violetred ")
+    _T("wheat white whitesmoke yellow yellowgreen");
+    
+const wxChar* DOTWordlist4 =
+    _T("URL arrowhead arrowsize arrowtail bb bgcolor bottomlabel center ")
+    _T("clusterrank color comment constraint decorate dir distortion ")
+    _T("fillcolor fixedsize fontcolor fontname fontsize group headclip ")
+    _T("headlabel headport height id label labelangle labeldistance ")
+    _T("labelfontcolor labelfontname labelfontsize layer layers margin ")
+    _T("mclimit minlen nodesep nslimit ordering orientation page pagedir ")
+    _T("peripheries port_label_distance quantum rank rankdir ranksep ratio ")
+    _T("regular rotate samehead sametail searchsize shape shapefile ")
+    _T("showboxes sides size skew style tailclip taillabel tailport ")
+    _T("toplabel weight width");
 
 //----------------------------------------------------------------------------
 //! languages
@@ -183,7 +297,7 @@ const LanguageInfo g_LanguagePrefs [] = {
       {mySTC_TYPE_COMMENT_LINE, NULL},
       {mySTC_TYPE_COMMENT_DOC, NULL},
       {mySTC_TYPE_NUMBER, NULL},
-      {mySTC_TYPE_WORD1, CppWordlist1}, // KEYWORDS
+      {mySTC_TYPE_WORD1, AMILabWordlist1}, // KEYWORDS
       {mySTC_TYPE_STRING, NULL},
       {mySTC_TYPE_CHARACTER, NULL},
       {mySTC_TYPE_UUID, NULL},
@@ -198,6 +312,43 @@ const LanguageInfo g_LanguagePrefs [] = {
       {mySTC_TYPE_WORD3, CppWordlist3}, // DOXY KEYWORDS
       {mySTC_TYPE_ERROR, NULL}, // KEYWORDS ERROR
       {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL},
+      {-1, NULL}},
+     mySTC_FOLD_COMMENT | mySTC_FOLD_COMPACT | mySTC_FOLD_PREPROC},
+    // AMILab
+    {_T("dot"),
+     _T("*.dot"),
+     wxSTC_LEX_CPP,
+     {{mySTC_TYPE_DEFAULT, NULL},
+      {mySTC_TYPE_COMMENT, NULL},
+      {mySTC_TYPE_COMMENT_LINE, NULL},
+      {mySTC_TYPE_COMMENT_DOC, NULL},
+      {mySTC_TYPE_NUMBER, NULL},
+      {mySTC_TYPE_WORD1, DOTWordlist1}, // KEYWORDS
+      {mySTC_TYPE_STRING, NULL},
+      {mySTC_TYPE_CHARACTER, NULL},
+      {mySTC_TYPE_UUID, NULL},
+      {mySTC_TYPE_PREPROCESSOR, NULL},
+      {mySTC_TYPE_OPERATOR, NULL},
+      {mySTC_TYPE_IDENTIFIER, NULL},
+      {mySTC_TYPE_STRING_EOL, NULL},
+      {mySTC_TYPE_DEFAULT, NULL}, // VERBATIM
+      {mySTC_TYPE_REGEX, NULL},
+      {mySTC_TYPE_COMMENT_SPECIAL, NULL}, // DOXY
+      {mySTC_TYPE_WORD2, DOTWordlist2}, // EXTRA WORDS
+      {mySTC_TYPE_WORD3, DOTWordlist3}, // DOXY KEYWORDS
+      {mySTC_TYPE_ERROR, NULL}, // KEYWORDS ERROR
+      {mySTC_TYPE_WORD4, DOTWordlist4}, // DOXY KEYWORDS
       {-1, NULL},
       {-1, NULL},
       {-1, NULL},
