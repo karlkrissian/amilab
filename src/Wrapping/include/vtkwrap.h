76b147944224923362f387545dcb46325bfac4d3
karl.krissian <karl.krissian@gmail.com>
2012-03-27 20:05:51 +0000

    moved vtkwrap to WrapVTKTools
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1715 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/Wrapping/include/vtkwrap.h
+++ src/Wrapping/include/vtkwrap.h
@@ -13,46 +13,46 @@
 #ifndef _VTKWRAP_H
 #define _VTKWRAP_H
 
-#include "WrapVTKConfigure.h"
+//#include "WrapVTKConfigure.h"
 
 //#include "surface.hpp"
 class InrImage;
 #include "surface.hpp"
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkMedianFilter3D( InrImage* im, int kx, int ky, int kz);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   amilab::SurfacePoly* Func_vtkMarchingCubes( InrImage* im, float Threshold);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   amilab::SurfacePoly* Func_decimate( amilab::SurfacePoly* , 
                                       float target_reduction=0.9);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   amilab::SurfacePoly* Func_vtkSmooth( amilab::SurfacePoly*, int numiter );
   
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   amilab::SurfacePoly* Func_vtkWindowedSinc( amilab::SurfacePoly*, int numiter );
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   void         Func_ApplyvtkSmooth( amilab::SurfacePoly* surf , int numiter);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkDist( InrImage* im);
 
 //InrImage*    Func_vtkPropDanielsson( InrImage* im, float dmax);
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkPropDanielsson( InrImage* im, float dmin, float dmax);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkPropDaniel2( InrImage* im, float threshold, 
                   float dmin, float dmax);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkConvexHull( InrImage* im, int order, float resolution);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkIsoContourDist( InrImage* im, float th);
 
 /* moved to ami::vtkFastMarchingFunctions
@@ -81,21 +81,21 @@ InrImage*    Func_vtkFastMarching( InrImage* input, InrImage* init,
                     int evol_scheme = 0);
 */
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkSignedBorgefors(InrImage* im, float dmax=10000);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkSignedFMDist(   InrImage* im, float dmax=10000);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkAnisoGS( InrImage* im, float sigma, float k, 
                                 float beta, int nb_iter,
                                 int nb_threads);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkDicomRead( const char*);
 
-WrapVTK_EXPORT 
+//WrapVTK_EXPORT 
   InrImage*    Func_vtkMINCRead( const char*);
 
 #endif // _VTKWRAP_H
