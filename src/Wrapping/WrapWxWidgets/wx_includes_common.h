4c1772c5006a968830a1e470c64f9b659ec550ca
karl.krissian <karl.krissian@gmail.com>
2012-06-18 17:08:40 +0000

    added missing files
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1780 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ src/Wrapping/WrapWxWidgets/wx_includes_common.h
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include "wxAmiEventHandler.h"
+
+#include <wx/aboutdlg.h>
+#include <wx/app.h>
+#include <wx/arrstr.h>
+#include <wx/artprov.h> 
+#include <wx/aui/aui.h>
+#include <wx/ffile.h>
+#include <wx/grid.h>
+#include <wx/object.h>
+#include <wx/platinfo.h>
+#include <wx/regex.h> 
+#include <wx/slider.h>
+#include <wx/stattext.h>
+#include <wx/tipdlg.h>
