5389015e1a2c087ce77830bf5efa565908bbb16f
karl.krissian <karl.krissian@gmail.com>
2012-10-09 10:35:11 +0000

    wrapping std::vector<double>, still to do: typedef
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1864 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/Wrapping/WrapCommon/common_includes.h
+++ src/Wrapping/WrapCommon/common_includes.h
@@ -3,8 +3,13 @@
 
 #include "amiImageExtent.h"
 
+#include <vector>
+
 #ifdef __GCCXML__
   template class ami::ImageExtent<float>;
+  template class std::vector<double>;
+  
+  typedef std::vector<double> vector_double;
 #endif // __GCCXML__
 
 // // trying iostream maybe should be moved from Algorithms to here ...
