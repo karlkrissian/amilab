9d4eb8cbcba5b026fe51cb2fe7a0aefb276d17df
karl.krissian <karl.krissian@gmail.com>
2012-10-22 18:19:53 +0000

    Wrapping of std::stringstream and std::vector<T>::iterator for T={float,int,double}
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1893 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/Wrapping/WrapCommon/classes_includes.py
+++ src/Wrapping/WrapCommon/classes_includes.py
@@ -1,6 +1,8 @@
 
 classes_includes = {
   "std::ostream"             : "iostream",
+  # need to improve this part to allow writting here std::stringstream ...
+  "std::basic_stringstream<char, std::char_traits<char>, std::allocator<char> >" : "sstream",
   }
   
   
--- src/Wrapping/WrapCommon/common_includes.h
+++ src/Wrapping/WrapCommon/common_includes.h
@@ -5,7 +5,9 @@
 
 #include <vector>
 #include <iostream>
+#include <istream>
 #include <fstream>
+#include <sstream>
 
 #ifdef __GCCXML__
   template class ami::ImageExtent<float>;
@@ -17,7 +19,14 @@
   typedef std::vector<float>  vector_float;
   typedef std::vector<int>    vector_int;
   
+  // instanciate iterators
+  vector_double::iterator vdi;
+  vector_float::iterator  vfi;
+  vector_int::iterator    vii;
+  
   //std::ostream std_ostream_obj;
+  std::stringstream string_stream_obj;
+//  std::iostream io_stream_obj;
 #endif // __GCCXML__
 
 // // trying iostream maybe should be moved from Algorithms to here ...
