61a8ac9bf9d272bdf4ce6ba6b2b8094466959ecb
karl.krissian <karl.krissian@gmail.com>
2012-10-22 20:29:44 +0000

    adding missing files for WrapITK compilation
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@1895 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

---
+++ src/Wrapping/WrapITK/members_blacklist.py
@@ -0,0 +1,5 @@
+
+members_blacklist=[
+   'gdcm::VL gdcm::DataSet::GetLength<gdcm::ExplicitDataElement>() const', # pb template function member?
+   'std::istream& gdcm::VL::Read<gdcm::SwapperDoOp>(std::istream&)', # idem
+]
