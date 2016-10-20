a1fc58b5d2227eccc3ef5a2d37a3c4c0ed0027a2
karl.krissian <karl.krissian@gmail.com>
2016-10-20 14:01:19 +0000

    [src/Wrapping/WrapCommon] defining include files for several std classes
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@2045 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/Wrapping/WrapCommon/classes_includes.py
+++ src/Wrapping/WrapCommon/classes_includes.py
@@ -7,5 +7,11 @@ classes_includes = {
   # need to improve this part to allow writting here std::stringstream ...
   "std::basic_stringstream<char, std::char_traits<char>, std::allocator<char> >"  : "sstream",
   "std::complex<float>"   : "complex",
+  "std::basic_istream" : "istream",
+  "std::basic_ostream" : "ostream",
+  "std::char_traits" : "string",
+  "std::allocator" : "memory",
+  "std::set" : "set",
+  "new_allocator" : "memory",
   }
   
