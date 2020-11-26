d93769a6cbd311a0eb8c8264c42d1ff342970f56
karl.krissian <karl.krissian@gmail.com>
2020-11-26 21:28:51 +0000

    Improve wx wrapping list of available variables/macros ...
    git-svn-id: file:///home/karl/SVN2/amilab.svn/code/trunk@2103 52b0d089-85ac-4fb5-acf5-4e3172ea7aaf

--- src/Wrapping/WrapWxWidgets/3.0/wx_includes.h
+++ src/Wrapping/WrapWxWidgets/3.0/wx_includes.h
@@ -39,29 +39,40 @@
   // declare classes to instantiate them
   // The correct number does not matter here, because it is for gccxml to 
   // to be able to instantiate the template class ...
-  int id=0; 
-  class wxEventTypeTag<wxCommandEvent>  et1(id);
-  class wxEventTypeTag<wxMouseEvent>    et2(id);
-  class wxEventTypeTag<wxKeyEvent>      et3(id);
-  class wxEventTypeTag<wxSizeEvent>     et4(id);
-  class wxEventTypeTag<wxPaintEvent>    et5(id);
-  class wxEventTypeTag<wxEraseEvent>    et6(id);
-  class wxEventTypeTag<wxMoveEvent>     et7(id);
-  class wxEventTypeTag<wxFocusEvent>    et8(id);
-  class wxEventTypeTag<wxCloseEvent>    et9(id);
-  class wxEventTypeTag<wxMenuEvent>     et10(id);
-  class wxEventTypeTag<wxTimerEvent>    et11(id);
-  class wxEventTypeTag<wxListEvent>     et12(id);
-  class wxEventTypeTag<wxHtmlCellEvent> et13(id);
-  class wxEventTypeTag<wxHtmlLinkEvent> et14(id);
+      int id=0;
+      class wxEventTypeTag<wxCommandEvent>  et1(id);
+      class wxEventTypeTag<wxMouseEvent>    et2(id);
+      class wxEventTypeTag<wxKeyEvent>      et3(id);
+      class wxEventTypeTag<wxSizeEvent>     et4(id);
+      class wxEventTypeTag<wxPaintEvent>    et5(id);
+      class wxEventTypeTag<wxEraseEvent>    et6(id);
+      class wxEventTypeTag<wxMoveEvent>     et7(id);
+      class wxEventTypeTag<wxFocusEvent>    et8(id);
+      class wxEventTypeTag<wxCloseEvent>    et9(id);
+      class wxEventTypeTag<wxMenuEvent>     et10(id);
+      class wxEventTypeTag<wxTimerEvent>    et11(id);
+      class wxEventTypeTag<wxListEvent>     et12(id);
+      class wxEventTypeTag<wxHtmlCellEvent> et13(id);
+      class wxEventTypeTag<wxHtmlLinkEvent> et14(id);
   // TODO list all the possible types of events here ...
   namespace wrap_wx {
-    enum
-    {
-      wxPATH_NO_SEPARATOR = 0x0000,  // for symmetry with wxPATH_GET_SEPARATOR
-      wxPATH_GET_VOLUME = 0x0001,  // include the volume if applicable
-      wxPATH_GET_SEPARATOR = 0x0002   // terminate the path with the separator
-    };
+
+    // force include headers in namespace
+    #undef _WX_TRACKER_H_
+    #undef _WX_REGEX_H_
+    #undef _WX_TIMER_H_BASE_
+    #undef _WX_FILENAME_H_
+
+    #include <wx/regex.h>
+    #include <wx/timer.h>
+    #include <wx/filename.h>
+
+//    enum
+//    {
+//      wxPATH_NO_SEPARATOR = 0x0000,  // for symmetry with wxPATH_GET_SEPARATOR
+//      wxPATH_GET_VOLUME = 0x0001,  // include the volume if applicable
+//      wxPATH_GET_SEPARATOR = 0x0002   // terminate the path with the separator
+//    };
 
   };
 #endif // __GCCXML__
